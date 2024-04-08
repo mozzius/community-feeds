2fad0f01996b545430462a7f8eaa75e9cbe3b613
Samuel Newman <mozzius@protonmail.com>
2024-04-08 15:30:26 +0100

    filter feeds to valid domains

--- src/subscription.ts
+++ src/subscription.ts
@@ -1,15 +1,21 @@
+import { BskyAgent } from '@atproto/api'
 import {
   OutputSchema as RepoEvent,
   isCommit,
 } from './lexicon/types/com/atproto/sync/subscribeRepos'
 import { FirehoseSubscriptionBase, getOpsByType } from './util/subscription'
 import { neon } from '@neondatabase/serverless'
+import cron from 'node-cron'
 
 type User = {
   did: string
   handle: string
   domain: string
 }
+
+const agent = new BskyAgent({
+  service: 'https://public.api.bsky.app',
+})
 
 export class FirehoseSubscription extends FirehoseSubscriptionBase {
   sql: ReturnType<typeof neon>
@@ -20,14 +26,57 @@ export class FirehoseSubscription extends FirehoseSubscriptionBase {
     super(a, b)
     this.sql = neon(connectionString)
     this.fetchDomains()
+
+    cron.schedule('*/15 * * * *', async () => {
+      try {
+        await this.fetchDomains()
+      } catch (e) {
+        console.log('error fetching domains')
+        console.error(e)
+      }
+    })
   }
 
   async fetchDomains() {
     this.lastFetched = Date.now()
-    const users = await this.sql(
+    const users = (await this.sql(
       'SELECT did, handle, "Domain".name AS "domain" FROM "User" JOIN "Domain" ON "User"."domainId" = "Domain".id',
+    )) as User[]
+
+    // split
+    const userChunks = users.reduce<User[][]>(
+      (acc, subject) => {
+        if (acc[acc.length - 1]!.length === 25) {
+          acc.push([subject])
+        } else {
+          acc[acc.length - 1]!.push(subject)
+        }
+        return acc
+      },
+      [[]],
     )
-    this.users = Object.values(users) as User[]
+
+    const validUsers = new Set<User>()
+
+    for (const chunk of userChunks) {
+      const {
+        data: { profiles },
+      } = await agent.getProfiles({
+        actors: chunk.map((user) => user.did),
+      })
+
+      for (const profile of profiles) {
+        const user = chunk.find((user) => user.did === profile.did)
+        if (!user) continue
+        if (profile.handle.endsWith(user.domain)) {
+          validUsers.add(user)
+        }
+      }
+    }
+
+    this.users = Array.from(validUsers)
+
+    console.log(users.length, this.users.length)
   }
 
   async handleEvent(evt: RepoEvent) {
